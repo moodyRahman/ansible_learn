ed8f74122337733a9f3086a2bbdaaefd8d2ed1b3
Moody Rahman <moody.byte@gmail.com>
2025-10-07 07:10:30 -0400

    init commit

---
+++ inventory_server.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python3
+from flask import *
+import docker
+
+
+client = docker.from_env()
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    containers = filter(lambda x: "shigechi" in x.name, client.containers.list())
+    out = {"d_containers":{"hosts":[x.name for x in containers]}}
+    return out
+
+if __name__ == "__main__":
+    app.run(debug=True)
