897c23218b783f3b845949c86165b9f6c88d65c7
Moody Rahman <moody.byte@gmail.com>
2025-10-07 09:20:54 -0400

    makefile

---
+++ Makefile
@@ -0,0 +1,14 @@
+
+inventory_server:
+	docker build -t moody/inventory_server:latest -f inventory_api_image/Dockerfile .;
+	docker compose up -d
+
+control_server:
+	docker build -t moody/cmoon:latest -f ./control_node_image/Dockerfile .
+	docker compose up -d
+
+stop:
+	docker compose down --timeout 0
+
+start: 
+	docker compose up -d
--- inventory_server.py
+++ inventory_server.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     containers = filter(lambda x: "shigechi" in x.name, client.containers.list())
-    out = {"d_containers":{"hosts":[x.name for x in containers]}}
-    return out
+    # out = {"d_containers":{"hosts":[x.name for x in containers]}}
+    return jsonify([x.name for x in containers])
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
